0f4892979fa6ad42740e69548b6828b402812445
Bill Shewan <bill.shewan@utiligent.ca>
2026-04-19 12:53:35 +1000

    Initial Commit

---
+++ Loopo/LoopoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopoApp.swift
+//  Loopo
+//
+//  Created by Bill Shewan on 19/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct LoopoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
